9b1908c384d30c0929f6307e1745f23b8690d74e
f3bruary <f3bruary@protonmail.ch>
2015-07-21 14:37:56 +0200

    improved spotify/cmus now playing

--- .conky/scripts/spotify.sh
+++ .conky/scripts/spotify.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-# Now Playing monitor for Spotify and/or CMUS
 
-spotify="$(echo `dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "artist"|egrep -v "artist"|egrep -v "array"|cut -b 27-|cut -d '"' -f 1|egrep -v ^$` "-" `dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "title"|egrep -v "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`)"
-cmus=`cmus-remote -Q | grep 'file' | cut -d '/' -f 5 | sed -e 's/\.mp3//g'`
-
-if [ $spotify == "-" ]; then
-    spotify="";
+if [[ -z $(pgrep spotify) ]]; then
+    true
 else
-    echo $spotify;
+    spotify="$(echo `dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "artist"|egrep -v "artist"|egrep -v "array"|cut -b 27-|cut -d '"' -f 1|egrep -v ^$` "-" `dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "title"|egrep -v "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`)"
+    echo $spotify
 fi
-    
-if [ $cmus -z ]; then
-    cmus="";
+
+if [[ -z $(pgrep cmus) ]]; then
+    true
 else
-    echo $cmus;
+    cmus=`cmus-remote -Q | grep 'file' | cut -d '/' -f 5 | sed -e 's/\.mp3//g'`
+    echo $cmus
 fi
